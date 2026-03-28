73aaa5b25cd7a99663b060a1b49a880e92342a75
steve <steve@cm.com>
2026-03-29 02:33:55 +1100

    feat: update formula to v0.2.0

--- Formula/orchstep.rb
+++ Formula/orchstep.rb
@@ -1,26 +1,26 @@
 class Orchstep < Formula
   desc "YAML-first workflow orchestration engine"
   homepage "https://orchstep.dev"
-  version "0.1.0"
+  version "0.2.0"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/orchstep/orchstep/releases/download/v0.1.0/orchstep_0.1.0_darwin_arm64.tar.gz"
-      sha256 "7d351b590c74de53b78cb78acc3b4fb8b8c25c21012c442592aa1ff9207647c8"
+      url "https://github.com/orchstep/orchstep/releases/download/v0.2.0/orchstep_0.2.0_darwin_arm64.tar.gz"
+      sha256 "0493c985c36db4eaf581f0b32cb090a9da51d5fa71f49dc11827d32cd31469f9"
     else
-      url "https://github.com/orchstep/orchstep/releases/download/v0.1.0/orchstep_0.1.0_darwin_amd64.tar.gz"
-      sha256 "1d44f6c337c60a1c2b2c19a1e6781a6bb304ac4d0650a6ce382900b0cfde17f2"
+      url "https://github.com/orchstep/orchstep/releases/download/v0.2.0/orchstep_0.2.0_darwin_amd64.tar.gz"
+      sha256 "cba3057fcdf9da86a10954b2aa78857df00f6d824bd3ee419e341d46312ff058"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/orchstep/orchstep/releases/download/v0.1.0/orchstep_0.1.0_linux_arm64.tar.gz"
-      sha256 "872bb528aa2205e6147dffbb9016ae5c9df359df04e28d133eb760ceb17b6a30"
+      url "https://github.com/orchstep/orchstep/releases/download/v0.2.0/orchstep_0.2.0_linux_arm64.tar.gz"
+      sha256 "441349bbd6b5d34797a6e3ca65ec3eacb478720a785a81ad43dca8a7ebf1f3b1"
     else
-      url "https://github.com/orchstep/orchstep/releases/download/v0.1.0/orchstep_0.1.0_linux_amd64.tar.gz"
-      sha256 "2c3c50bfdd8dd01dd57b055ae3a523ea4c33037a27d5d7f5efe588dbf8e34cf7"
+      url "https://github.com/orchstep/orchstep/releases/download/v0.2.0/orchstep_0.2.0_linux_amd64.tar.gz"
+      sha256 "e3756af13054b31e0f1244f830094eb33d0260c870d32740614f24c284119865"
     end
   end
 
